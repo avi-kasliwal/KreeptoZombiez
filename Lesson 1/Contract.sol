404b99ee1c9067ea496c6c877692c3e4b2138855
avi-kasliwal <avi.kasliwal04@gmail.com>
2022-01-29 22:56:21 +0530

    Lesson 2 Chapter 2

--- Lesson 1/Contract.sol
+++ Lesson 1/Contract.sol
@@ -14,6 +14,9 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    // Map to store ownership of a zombie created.
+    mapping(uint256 => address) public zombieToOwner;
+
     function _createZombie(string memory _name, uint256 _dna) private {
         zombies.push(Zombie(_name, _dna));
         uint256 id = zombies.length - 1;
