ccc344bdc3698c0c144647e7dc718a1e2137bbc4
avi-kasliwal <avi.kasliwal04@gmail.com>
2022-01-30 09:17:46 +0530

    Lesson 2 Chapter 5

---
+++ Lesson 2/Contract.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.8.0;
+
+contract ZombieFactory {
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+
+    // tell front-end that some event has happened in blockchain
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+
+    Zombie[] public zombies;
+
+    // Map to store ownership of a zombie created.
+    mapping(uint256 => address) public zombieToOwner;
+
+    // Map to get number of zombies for an account address.
+    mapping(address => uint256) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint256 _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        uint256 id = zombies.length - 1;
+
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+
+        // trigger the event
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        // keccak256 returns a 256 bit number.
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+
+        // since we only want 16 digit long number.
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        // To make sure that there is no zombie created for a given account
+        require(ownerZombieCount[msg.sender] == 0);
+
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
+
+// ZombieFeeding inherits ZombieFactory
+contract ZombieFeeding is ZombieFactory {
+
+}
